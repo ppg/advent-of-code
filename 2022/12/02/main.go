e1855422f8b93792134089e9b4896e79e0f2e547
Peter P. Gengler <peter.gengler@jumpcloud.com>
2022-12-02 11:03:58 +0530

    2022-12-02: Update to solve second part

--- 2022/12/02/main.go
+++ 2022/12/02/main.go
@@ -8,7 +8,11 @@ import (
 )
 
 func main() {
-	readFile, err := os.Open("input.txt")
+	input := "input.txt"
+	if len(os.Args) > 1 {
+		input = os.Args[1]
+	}
+	readFile, err := os.Open(input)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -20,13 +24,28 @@ func main() {
 	fileScanner.Split(bufio.ScanLines)
 	fmt.Printf("%10s %10s %5s\n", "opponent", "self", "score")
 	for fileScanner.Scan() {
+		// Get the entries from the line
 		line := strings.TrimSpace(fileScanner.Text())
-		plays := strings.Split(line, " ")
-		if len(plays) != 2 {
+		cols := strings.Split(line, " ")
+		if len(cols) != 2 {
 			panic(fmt.Errorf("unexpected input: %s", line))
 		}
-		opponent := opponentPlays[plays[0]]
-		self := selfPlays[plays[1]]
+		// The opponent's play is always the first column
+		opponent := opponentPlays[cols[0]]
+
+		// Get the self play depending on which question part
+		var self Play
+		if os.Getenv("PART") == "2" {
+			// in part 2 the second column is interpreted as the desired outcome, so
+			// lookup the self play in a map of outcomes from desired outcome to what
+			// the opponent has
+			self = outcomePlays[cols[1]][opponent]
+		} else {
+			// in part 1 the second column is interpreted as the self play, so lookup the self play in it's map
+			self = selfPlays[cols[1]]
+		}
+
+		// Score self play and the outcome and accumulate
 		score := playScores[self] + vsScores[self][opponent]
 		total += score
 
@@ -44,6 +63,28 @@ var selfPlays = map[string]Play{
 	"X": Rock,
 	"Y": Paper,
 	"Z": Scissors,
+}
+
+// The Elf finishes helping with the tent and sneaks back over to you. "Anyway,
+// the second column says how the round needs to end: X means you need to lose,
+// Y means you need to end the round in a draw, and Z means you need to win.
+// Good luck!"
+var outcomePlays = map[string]map[Play]Play{
+	"X": map[Play]Play{ // lose
+		Rock:     Scissors,
+		Paper:    Rock,
+		Scissors: Paper,
+	},
+	"Y": map[Play]Play{ // draw
+		Rock:     Rock,
+		Paper:    Paper,
+		Scissors: Scissors,
+	},
+	"Z": map[Play]Play{ // win
+		Rock:     Paper,
+		Paper:    Scissors,
+		Scissors: Rock,
+	},
 }
 
 var playScores = map[Play]int{
