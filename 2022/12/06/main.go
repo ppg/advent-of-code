1b1d60d6f9df4aff4cff9d756a3481eb999c60f7
Peter P. Gengler <peter.gengler@jumpcloud.com>
2022-12-06 10:50:09 +0530

    2022-12-06: First pass at part 2

--- 2022/12/06/main.go
+++ 2022/12/06/main.go
@@ -19,17 +19,22 @@ var parser = framework.LineParser
 
 func solution0(w io.Writer, runner *framework.Runner[string]) {
 	datastream := <-runner.Lines()
-	for i := 4; i < len(datastream); i++ {
-		// naive solution to check each 4 char packet
+	var markerSize int
+	runner.ByPart(
+		func() { markerSize = 4 },
+		func() { markerSize = 14 },
+	)
+	for i := markerSize; i < len(datastream); i++ {
+		// naive solution to check each markerSize char packet
 		// TODO: when we check the packet we can jump farther ahead depending on where repeats are;
 		// i.e. abccdef after checking abcc we can move to cdef right away
-		packet := datastream[i-4 : i]
+		packet := datastream[i-markerSize : i]
 		fmt.Fprintf(w, "inspecting packet: %s\n", packet)
-		m := make(map[rune]bool, 4)
+		m := make(map[rune]bool, markerSize)
 		for _, char := range packet {
 			m[char] = true
 		}
-		if len(m) == 4 {
+		if len(m) == markerSize {
 			fmt.Fprintf(w, "found start-of-packet %s: %d\n", packet, i)
 			return
 		}
