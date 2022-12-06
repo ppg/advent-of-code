76fd3df07aeb798fa796cf3e73731ee2f5bba8d9
Peter P. Gengler <peter.gengler@jumpcloud.com>
2022-12-06 11:30:27 +0530

    2022-12-06: Cleanup solution 1 to make a little clearer

--- 2022/12/06/main.go
+++ 2022/12/06/main.go
@@ -52,20 +52,24 @@ func solution1(w io.Writer, runner *framework.Runner[string]) {
 		func() { markerSize = 4 },
 		func() { markerSize = 14 },
 	)
-	i := markerSize
-	for i <= len(datastream) {
+
+	// Allow variable skips depending on where the matching char is
+	// i.e. abccdef after checking abcc we can move to cdef right away (skip 3)
+	// i.e. cabcdef after checking cabc we can move to abcd right away (skip 1)
+	var skip int
+	for i := markerSize; i < len(datastream); i += skip {
 		packet := datastream[i-markerSize : i]
 		fmt.Fprintf(w, "inspecting packet: %s\n", packet)
 
 		// Search for the latest repeated char and advance to that position
-		// i.e. abccdef after checking abcc we can move to cdef right away
 		var dupes bool
 		m := make(map[byte]bool, markerSize)
 		for j := len(packet) - 1; j >= 0; j-- {
 			char := packet[j]
 			if m[char] {
-				// advance 1 past where we found the matching char since we don't need to retest that
-				i = i + j + 1
+				// advance 1 past where we found the matching char since we don't need
+				// to retest that char
+				skip = j + 1
 				dupes = true
 				break
 			}
@@ -75,6 +79,11 @@ func solution1(w io.Writer, runner *framework.Runner[string]) {
 		if !dupes {
 			fmt.Fprintf(w, "found start-of-packet %s: %d\n", packet, i)
 			return
+		}
+
+		// assert we don't have a 0 skip and will redo work
+		if skip == 0 {
+			panic("invalid skip, infinite recursion")
 		}
 	}
 	fmt.Fprintf(w, "could not find start-of-packet\n")
