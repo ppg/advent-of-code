d6b34d0f406b0595d685e85de2a121ebcee82927
Peter P. Gengler <peter.gengler@jumpcloud.com>
2022-12-08 00:25:51 +0530

    2022-12-07: Add README and test

--- 2022/12/07/main_test.go
+++ 2022/12/07/main_test.go
@@ -6,7 +6,4 @@ import (
 	framework "github.com/ppg/advent-of-code/2022/12/framework"
 )
 
-func BenchmarkSolution0Part1(b *testing.B) { framework.Benchmark(b, parser, solution0, 1) }
-func BenchmarkSolution0Part2(b *testing.B) { framework.Benchmark(b, parser, solution0, 2) }
-func BenchmarkSolution1Part1(b *testing.B) { framework.Benchmark(b, parser, solution1, 1) }
-func BenchmarkSolution1Part2(b *testing.B) { framework.Benchmark(b, parser, solution1, 2) }
+func BenchmarkSolution0(b *testing.B) { framework.Benchmark(b, parser, solution0, 1) }
